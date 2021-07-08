4012df59b0357968f580c76fd1991ed1da9df964
WenShuang Lu <wenshuang_lu@163.com>
2021-07-08 16:06:26 +0800

    rename file path

---
+++ build-hugo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ "$1" == "" ];then
+  echo "Usage: $0 websource-hugo-path"
+  exit 1
+fi
+
+if [ ! -d $1 ];then
+  echo "Error: the dir $1 not exist"
+  exit 1
+fi
+
+rm -rf ./docs
+cd $1
+hugo
+mv public ../docs
+cd ..
+
--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd websource
-hugo
-mv public/* ../
-rm -rf public
-cd ..
-
