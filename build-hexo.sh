f0691552ef8fe31be6b3967ad86cf4f0b477d138
WenShuang Lu <wenshuang_lu@163.com>
2021-08-11 14:39:20 +0800

    hexo: use html tag to validate the site

--- build-hexo.sh
+++ build-hexo.sh
@@ -17,3 +17,10 @@ hexo g
 mv public ../docs
 cd ..
 set +x
+
+###### 搜索引擎 SEO
+# github.io 无法使用百度，目前只配置了 google、bing
+# 不知为何，无法正常工作；在主题 _config.yml 中配置 google-site-verification ，会采用HTML标签方式
+cp google557be7742b81daa1.html docs/
+# 非常简单，很好用
+cp BingSiteAuth.xml docs
